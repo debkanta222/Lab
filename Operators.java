3be41444937878f64828b0646878e774232e23c2
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-12 17:35:11 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,30 @@
+//Write a java program to print the sum(addition),multiply,substract,divide and
+//remainder of two numbers. 
+
+class Operators{
+    public static void main(String[] args) {
+    System.out.println("the two numbers are:");
+        int a,b,c;
+        a=125;
+        b=24;
+        System.out.println(a+" "+b);
+        System.out.println("the result  of multiplication is:");
+        c=a*b;//logic for multiplication
+        System.out.println(c);
+        System.out.println("the result of division is:");
+        c=a/b;//logic for division
+        System.out.println(c);
+        System.out.println("the result of substraction is:");
+        c=a-b;//logic for substraction
+        System.out.println(c);
+        System.out.println("the result of addition is:");
+        c=a+b;//logic for addition
+        System.out.println(c);
+        System.out.println("the result of remainder is:");
+        c=a%b;//logic for remainder
+        System.out.println(c);
+
+
+    }
+    
+}
