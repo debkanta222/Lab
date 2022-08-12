57947aaf1b547931e904015ec57e0d4b3e3ad35d
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-12 18:06:05 +0530

    Update Swapping.java

--- Swapping.java
+++ Swapping.java
@@ -3,12 +3,12 @@ class Swapping{
 public static void main(String[]args){
 System.out.println("the numbers are:");
 int a,b,c;
-a=10;
-b=20;
-System.out.println(a+" , "+b);
-c=a;
-a=b;
-b=c;
-System.out.println("After swapping,the result is:" + a + " " + b);
+a=10;//1st number as input
+b=20;//2nd number as input
+System.out.println(a+" , "+b);//printing the input numbers
+c=a;//swapping the numbers
+a=b;//swapping the numbers
+b=c;//swapping the numbers
+System.out.println("After swapping,the result is:" + a + " " + b);//printing the result after swapping
 }
 }
