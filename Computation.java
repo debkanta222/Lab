9b5f81fc7216e3293a56f96748a4f193dd639e9c
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-12 17:34:08 +0530

    Add files via upload

---
+++ Computation.java
@@ -0,0 +1,15 @@
+//Write a java program to compute the specified 
+//expressions and print the output.
+
+class Computation{
+public static void main(String[]args){
+float a,b,c,d,ans;
+a=25.5f;
+b=3.5f;
+c=40.5f;
+d=4.5f;
+ans=(a*b-b*b)/(c-d);
+System.out.println(ans);
+}
+}
+
