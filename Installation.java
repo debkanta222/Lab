1b42f3f91b79fc5a8753f20173eb26f0642f3b3e
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-12 18:02:17 +0530

    Update Installation.java

--- Installation.java
+++ Installation.java
@@ -2,11 +2,11 @@
 
 class Installation{
 public static void main (String[]args){
-System.out.println("\nJava Version:"+System.getProperty("java.version"));
-System.out.println("Java Runtime Version:"+System.getProperty("java.runtime.version"));
-System.out.println("Java Home:"+System.getProperty("java.home"));
-System.out.println("Java Vendor:"+System.getProperty("java.vendor"));
-System.out.println("Java Vendor URL:"+System.getProperty("java.vendor.url"));
-System.out.println("Java Class Path:"+System.getProperty("java.class.path")+"\n");
+System.out.println("\nJava Version:"+System.getProperty("java.version"));//printing java version
+System.out.println("Java Runtime Version:"+System.getProperty("java.runtime.version"));//printing jre version
+System.out.println("Java Home:"+System.getProperty("java.home"));//printing java home//printing java home
+System.out.println("Java Vendor:"+System.getProperty("java.vendor"));//printing java vendor
+System.out.println("Java Vendor URL:"+System.getProperty("java.vendor.url"));//printing java vendor url
+System.out.println("Java Class Path:"+System.getProperty("java.class.path")+"\n");//printing java classpath
 }
 }
