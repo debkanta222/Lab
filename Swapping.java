d3fd6321a20de81146dcd28b2c22d59968b68001
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-12 17:35:29 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,14 @@
+//swapping two vaeriables
+class Swapping{
+public static void main(String[]args){
+System.out.println("the numbers are:");
+int a,b,c;
+a=10;
+b=20;
+System.out.println(a+" , "+b);
+c=a;
+a=b;
+b=c;
+System.out.println("After swapping,the result is:" + a + " " + b);
+}
+}
