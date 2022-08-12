e98e5980873ecc60c289f66008c6ec69e46f283f
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-12 18:00:01 +0530

    Update InputDemo.java

--- InputDemo.java
+++ InputDemo.java
@@ -4,16 +4,16 @@
 class InputDemo{
 public static void main(String[]args){
 System.out.println("the numbers are:");
-int a,b,c,d,e;
+int a,b,c,d,e;//variable decleration
 a=5;
 b=10;
 c=15;
 d=20;
 e=25;
 int average;
-System.out.println(a+ "," + b + "," + c + "," + d + "," +e);
-average=(a+b+c+d+e)/5;
-System.out.println("the average is :" +average);
+System.out.println(a+ "," + b + "," + c + "," + d + "," +e);//printing the numbers
+average=(a+b+c+d+e)/5;//logic to find average
+System.out.println("the average is :" +average);//printing the average
 }
 }
 
