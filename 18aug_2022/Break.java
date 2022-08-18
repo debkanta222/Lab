1b9205919c86faadf1e4a82918608e3fe4f0ed50
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-18 17:40:53 +0530

    Add files via upload

---
+++ 18aug_2022/Break.java
@@ -0,0 +1,14 @@
+//Write a java program to break an integer into a sequence of individual digits.
+
+package Activity;
+
+public class Break {
+
+	public static void main(String[] args) {
+		int a=2441139;//initialization of variable a
+		System.out.println("the sample input is:"+ a);
+		System.out.println("2" + " "+ "4" +" " + "4" + " "+ "1" +" "+"1" +" "+ "3" +" "+ "9");
+
+	}
+
+}
